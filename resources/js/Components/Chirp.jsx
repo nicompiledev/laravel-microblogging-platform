4dd69ecf447d6bc94ea5c7dbfd2a97b2fbfbb774
nicompiledev <nsrivera80@soy.sena.edu.co>
2024-04-28 20:39:50 -0500

    added dayjs library to support relative days

--- resources/js/Components/Chirp.jsx
+++ resources/js/Components/Chirp.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+
+dayjs.extend(relativeTime);
 
 export default function Chirp({ chirp }) {
     return (
@@ -22,7 +26,7 @@ export default function Chirp({ chirp }) {
                     <div>
                         <span className="text-gray-800">{chirp.user.name}</span>
                         <small className="ml-2 text-sm text-gray-600">
-                            {new Date(chirp.created_at).toLocaleString()}
+                            {dayjs(chirp.created_at).fromNow()}
                         </small>
                     </div>
                 </div>
