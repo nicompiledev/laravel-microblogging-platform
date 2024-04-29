4c38c61645334fea3d82bcab2078ef0f06876341
nicompiledev <nsrivera80@soy.sena.edu.co>
2024-04-28 20:33:00 -0500

    added support to show the created chirps

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Chirp;
 use App\Http\Controllers\Controller;
 
+use Illuminate\Http\RedirectRequest;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -19,10 +20,26 @@ class ChirpController extends Controller
 
     public function index(): Response
     {
-        
+
         return Inertia::render('Chirps/Index', [
             //
+            'chirps' => Chirp::with('user:id,name')->latest()->get(),
+            
         ]);
     }
+
+    public function store(Request $request): RedirectResponse
+    {
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+
+        $request->user()->chirps()->create($validated);
+
+        return redirect()->route('chirps.index');
+    }
+
+
+
 
 }
--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -3,9 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Model;
 
 class Chirp extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['message'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
